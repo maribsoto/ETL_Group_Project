135edcca2d402e02a59c5685b3b4ca0eb6a2736b
aradeleke <59860768+aradeleke@users.noreply.github.com>
2020-04-27 23:17:46 -0500

    Update etl_emply_ins.sql

--- etl_emply_ins.sql
+++ etl_emply_ins.sql
@@ -1,31 +1,12 @@
+Column: public.employment.state
+
+-- ALTER TABLE public.employment DROP COLUMN state;
+
+ALTER TABLE public.employment
+    ADD COLUMN state character varying COLLATE pg_catalog."default";
+	
+	
+select 'ownership', 'Industry' from employment
+where state =  'Texas'
+
 select * from employment
-
-CREATE TABLE unemployment (
-    State varchar(20) ,
-    labor_force varchar(100) ,
-    total_unemp varchar(100) ,
-    ins_unemp varchar(100) 
-);
-
-CREATE TABLE employment (
-    State varchar ,
-    ownership varchar(100) ,
-    Industry VARCHAR ,
-    est_count VARCHAR ,
-    tot_q1_wages VARCHAR
-    
-);
-
-CREATE TABLE us_employment (
-    country VARCHAR ,
-    ownership varchar(100) ,
-    Industry VARCHAR ,
-    est_count VARCHAR ,
-    tot_q1_wages VARCHAR
-);
-
-
-SELECT employment.state, employment.Ownership, employment.est_count, 
-FROM employment
-JOIN unemployment
-ON unemployment.State = unemployment.labor_force;
